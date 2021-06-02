02d0848230929f7e1dd678be12d5b9402d98ad8c
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-06-02 16:04:38 +0530

    style changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div className="text-center animate-bounce text-6xl text-gray-600 font-bold pt-10">
-            <span className="animate-ping "> 
+            <span className="animate-pulse "> 
             P i x G a l l e r y
             </span>
         </div>
